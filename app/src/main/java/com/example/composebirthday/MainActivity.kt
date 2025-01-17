8476de90d78f57c2e64f40c8c6f17aab24ec5814
Don Michael <donmichael21@gmail.com>
2025-01-17 22:05:51 +0200

    Added Column

--- app/src/main/java/com/example/composebirthday/MainActivity.kt
+++ app/src/main/java/com/example/composebirthday/MainActivity.kt
@@ -9,16 +9,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composebirthday.ui.theme.ComposeBirthdayTheme
-import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,11 +47,15 @@ fun WagwanText(modifier: Modifier = Modifier, message: String, from: String) {
         Text(
             text = message,
             fontSize = 100.sp,
-            lineHeight = 116.sp
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
     
